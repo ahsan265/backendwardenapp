c30662b6b912cb49748569458339e248c3d4a316
AMNASHAMSHAD1 <47166181+AMNASHAMSHAD1@users.noreply.github.com>
2020-10-31 14:14:11 +0500

    Update user.route.js

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { fetchAllReportsFromWarden, fetchOneReports } from '../controller/admin';
+import { fetchAllReportsFromWarden, fetchOneReports,fetchReportsByLocation,fetchReportsByWarden,fetchReportsBytimeDuration,fetchReportsByRoadBlock,fetchReportsByTrafficincident } from '../controller/admin';
 
 export const userRoute = express.Router()
 
@@ -9,3 +9,11 @@ userRoute.get(`${BASE_URL}/:id/fetch-report`,  fetchOneReports);
 
 
 userRoute.get(`${BASE_URL}/fetch-allReports`, fetchAllReportsFromWarden);
+
+userRoute.get(`${BASE_URL}/:loc/fetch-allReportsByLocation`, fetchReportsByLocation);
+userRoute.get(`${BASE_URL}/:ward/fetch-allReportsByWarden`, fetchReportsByWarden);
+userRoute.get(`${BASE_URL}/:time/fetch-allReportsBytimeDuration`, fetchReportsBytimeDuration);
+userRoute.get(`${BASE_URL}/:Rb/fetch-allReportsByRoadBlock`, fetchReportsByRoadBlock);
+userRoute.get(`${BASE_URL}/:incident/fetch-allReportsByTrafficincident`, fetchReportsByTrafficincident );
+
+
